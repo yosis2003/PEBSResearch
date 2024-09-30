fa544dc858c9b9b04167b709f076c9c9d121c21a
yosis2003 <yousif_fadi@hotmail.com>
2024-09-30 11:32:11 -0700

    tester done

--- testeralgorithm.c
+++ testeralgorithm.c
@@ -2,32 +2,54 @@
 #include <stdio.h>
 
 #define MAXELEMS 100000
-#define PATTERN 20
+#define FIND 20
 
 
 
 int generateSomePattern(int *pattern)
 {
-    for (int i = 0; i < PATTERN; i++)
-    {
-        pattern[i] = rand() % (10 + 1 - 0) + 0;
-    }
+
 }
 
 
 int main()
 {
     int array[MAXELEMS];
-    int pattern[PATTERN];
+    int pattern[FIND];
+    int amountFound = 0;
 
-    generateSomePattern(pattern);
+    // to ensure that the same numbers get generated every time
+    srand(42);
+    for (int i = 0; i < FIND; i++)
+    {
+        pattern[i] = rand() % (10 + 1 - 1) + 1;
+        printf("%d\n", pattern[i]);
+    }
+
+    // generateSomePattern(pattern);
 
     for (int i = 0; i < MAXELEMS; i++)
     {
         array[i] = rand();
-        printf("%d\n", array[i]);
+        // printf("%d\n", array[i]);
     }
-    
+
+    for (int i = 0; i < FIND; i++)
+    {
+        int lookFor = pattern[i];
+        amountFound = 0;
+
+        for (int j = 0; j < MAXELEMS; j++)
+        {
+            if (array[i] % lookFor == 0)
+            {
+                array[i] = rand();
+                amountFound += 1;
+            }
+            
+        }
+        printf("%d\n", amountFound);
+    }
         
 
 }
